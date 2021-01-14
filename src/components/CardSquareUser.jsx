3392084e8abd2592856e69c93c374136d3edb460
Minirion <deschampsm.pro@gmail.com>
2021-01-14 15:54:01 +0100

    new competence

--- src/components/CardSquareUser.jsx
+++ src/components/CardSquareUser.jsx
@@ -4,18 +4,6 @@ import styled from 'styled-components';
 import markbois from "../img/markbois.png";
 import user from "../img/user1.jpeg";
 
-const Status = styled.input`
-  width: 6em;
-  height: 1.4em;
-  left: 48px;
-  top: 288px;
-  background: #F6D398;
-  border-radius: 50px;
-  border:none;
-  color: white;
-  font-size: 60%;
-`
-
 const WrapCard = styled.div`
   width: 7em;
   height: 8em;
@@ -26,53 +14,76 @@ const WrapCard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  `
+  const WrapItems = styled.div`
+  display: flex,
+  align-items: center;
 `
-const WrapItem = styled.div`
+
+  const WrapMachine = styled.div`
   margin: auto;
   background: #FFFFFF;
   border-radius: 10px;
   display: flex;
   align-items: center;
 `
+const WrapLevel = styled.div`
+`
+
+const WrapCompetence = styled.div`
+  background: #FFFFFF;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+`
+
 const Img = styled.img`
+  width: 110%;
+  z-index:10;
   position: absolute;
-  width: 120%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  &:hover {
-    filter: grayscale(90%);
-    width: 170%;
-    transition-property: filter width;
-    transition-duration: 0.3s;
-  }
+  bottom: 5%;
 `
 const Machine = styled.img`
   width: 1.5em;
 `
+const ImgMark = styled.img`
+  width: 1em;
+  z-index: 11;
+  position: absolute;
+  margin: 0 0 0 5.8em;
+`
 const ProjetTitle = styled.h2`
   font-size: 65%;
+  color: #606060;
 `
 const WrapImg = styled.div`
   position: relative;
   overflow: hidden;
   width: 7em;
-  height: 4em;
+  height: 5em;
 `
 
 const cardSquareUser = () => {
   return (
     <WrapCard>
       <WrapImg>
-        <img src={markbois} alt="markerBois" />
+      <ImgMark src={markbois} alt="markerBois" />
       <Img src={user} alt="User"/>
       </WrapImg>
-      <ProjetTitle>Nom de projet</ProjetTitle>
-      <WrapItem>
-        <Status type="button" value="en cours"/>
+      <ProjetTitle>Nom de la personne</ProjetTitle>
+      <WrapItems>
+      <WrapCompetence>
+        <WrapLevel>
+          <Machine src={couture} alt="Machine"/>
+          <Machine src={couture} alt="Machine"/>
+        </WrapLevel>
+      </WrapCompetence>
+      <WrapMachine>
         <Machine src="https://artilect.fr/wp-content/uploads/doliconnect/product/183/LASER-SP100-Plan%20de%20travail%2080-200x200.png" alt="Machine"/>
-      </WrapItem>
+        <Machine src="https://artilect.fr/wp-content/uploads/doliconnect/product/183/LASER-SP100-Plan%20de%20travail%2080-200x200.png" alt="Machine"/>
+      </WrapMachine>
+      </WrapItems>
     </WrapCard>
   );
 };
