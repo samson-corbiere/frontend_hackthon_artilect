b2fbec5683680b673c1c870a9b2f923290957609
lilykr <lisa-lou.k@hotmail.fr>
2021-01-14 12:19:42 +0100

    added Profile in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 import Chat from "./components/Chat";
-import Machine from "./components/Machine";
-import Profile from "./components/Profile";
+import Machine from "./components/Machine"
+import Profile from "./components/Profile"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div className="application">
         <Chat/>
         <Machine />
         <Profile />
+
       </div>
     );
   }
