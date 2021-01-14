851709d043f212dfa80e0c2e6ea8b966fa047682
Jessica GIRAUD <kikaaaa@MacBook-Pro.lan>
2021-01-14 16:25:23 +0100

    photo_maker

--- src/components/PhotoMaker.jsx
+++ src/components/PhotoMaker.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import "./styles/PhotoMaker.css";
+import "../styles/PhotoMaker.css";
 
 function PhotoMaker ({personality_name, personality_url, photo, user_name}) {
   return (
     <div className='container'>
       <img src={photo} alt={user_name} className="photo-user" />
-      <img src={personality_url} alt={personality_name} className="wood" />
+      <img src={personality_url} alt={personality_name} className="matiere" />
     </div>
   );
 }
