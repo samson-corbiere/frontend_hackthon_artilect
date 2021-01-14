d515cb37e28ef7793c57d9d37f97334fcf22a8a2
loulou <yourdj.france@gmail.com>
2021-01-14 17:02:26 +0100

    working on display machinne component

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
 import "../styles/Accueil.css";
+import Machine from "../components/Machine";
 
 class Accueil extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class Accueil extends React.Component {
       <div className="background-home">
         <Router>
           <div className="main">
+            <Machine></Machine>
             <div>je suis la page d'accueil</div>
           </div>
         </Router>
--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Button } from "react";
 import machineDataJson from "../static/machine.json";
 import PhotoMaker from "../components/PhotoMaker";
 
@@ -10,14 +10,32 @@ function Machine() {
     setProfile(machine);
   }, []);
 
+  // TODO ==> FWHEN WE CALL MACHINE COMPONENT, GIVE ID OF MACHINE IN PROPS
+  // FOR RETRIEVE THE GOOD PAGE OF MACHINE
+
   return (
     <div>
-      {machineData.map(item => (
-        <PhotoMaker
-          user_name={item.name}
-          photo={item.url_photo}
-        />
-      ))}
+      {machineData.map(item => {
+        {
+          console.log("in machine 1");
+          console.log("item name ==> " +item.name);
+        }
+        // <p>{item.name}</p>
+        <PhotoMaker user_name={item.name} photo={item.url_photo} />;
+          <div>
+            <p>{item.name}</p>
+            <p>{item.notice}</p>
+            <ul>
+              <li></li>
+            </ul>
+          </div>;
+
+        // TODO change IF condition with id from props
+        if (item.id === 1) {
+          
+          
+        }
+      })}
     </div>
   );
 }
