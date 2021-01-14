3bd03bd9b6cc34aaa4edeca52d38764cb910e8a9
loulou <yourdj.france@gmail.com>
2021-01-14 13:57:39 +0100

    little change on app.js

--- src/App.jsx
+++ src/App.jsx
@@ -23,23 +23,17 @@ class App extends React.Component {
     return (
       <Router>
         <div className="main">
-          {/* <p>je suis la page d'accueil</p> */}
           <Chat />
           <main>
-            {" "}
-            {/* accueil machine projets communauté  direct */}
             <Switch>
               <Route exact path="/Accueil" component={Accueil}></Route>
               <Route exact path="/Machines" component={Machine}></Route>
               <Route exact path="/Projets" component={Projects}></Route>
               <Route exact path="/Communaute" component={Communaute}></Route>
               <Route exact path="/Direct" component={Direct}></Route>
-
             </Switch>
           </main>
           <BottomNavigation />
-
-          {/* <footer>A&F Développement</footer> */}
         </div>
       </Router>
     );
