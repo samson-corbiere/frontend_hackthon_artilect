53ec345f4a2a5047c28e2bceb6da91d67c4706d1
Minirion <deschampsm.pro@gmail.com>
2021-01-15 02:45:52 +0100

    add some css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import logo from "../img/logo.png";
+import hello from "../img/hello.png";
+import deco from "../img/deco.png";
 
 const WrapHeader = styled.div`
   display: flex;
   color-background: whrite;
   align-items: center;
-  align-content: space-around
+  align-content: space-around;
+  height:4em;
 `
 
-const Header = () => {
-<WrapHeader>
+const Logo = styled.img`
+  height: 3em;
+  margin: 1em;
+  filter: grayscale(60%);
+`
+const Hello = styled.img`
+  height: 1.5em;
+  margin: 1em;
+`
+const Deco = styled.img`
+  height: 1.5em;
+  margin-left: 5.5em;
+`
 
+
+const Header = () => {
+  return (
+<WrapHeader>
+<Logo src={logo} alt="logo"/>
+<p>| Hello John ! |</p>
+<Hello src={hello} alt="logo"/>
+<Deco src={deco} alt="deco"/>
 </WrapHeader>
-}
+)
+  }
 
 export default Header;
