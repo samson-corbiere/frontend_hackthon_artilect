87050734452ae0ba7afad53d50775ec26cb9f91d
Jessica GIRAUD <kikaaaa@MacBook-Pro.lan>
2021-01-15 09:03:05 +0100

    merge from dev

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -4,7 +4,7 @@ import PrincipalButton from "../components/PrincipalButton";
 import PrincipalTitle from "../components/PrincipalTitle"
 import styled from 'styled-components';
 import { useParams } from "react-router-dom";
-import PhotoMaker from "../components/photoMachine";
+import PhotoMaker from "../components/PhotoMaker";
 import "../styles/projet.css"
 
 
